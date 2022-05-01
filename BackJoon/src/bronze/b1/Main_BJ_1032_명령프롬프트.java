f59ff0df90b7300b9a8137d18144906a6b011f27
jhno96 <jhno96@gmail.com>
2022-05-01 23:32:24 +0900

    update

---
+++ BackJoon/src/bronze/b1/Main_BJ_1032_명령프롬프트.java
@@ -0,0 +1,40 @@
+package bronze.b1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author : jhno96
+ * @date : 2022-04-29
+ */
+public class Main_BJ_1032_명령프롬프트 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        String[] input = new String[N];
+        for (int i = 0; i < N; i++) {
+            input[i] = br.readLine();
+        }
+
+        boolean flag = true;
+        for (int i = 0; i < input[0].length(); i++) {
+            char c = input[0].charAt(i);
+
+            flag = true;
+            for (int j = 1; j < N; j++) {
+                if(c != input[j].charAt(i)){
+                    flag = false;
+                    break;
+                }
+            }
+            sb.append(flag ? c : '?');
+        }
+        
+        System.out.println(sb.toString());
+    }
+
+}
---
+++ BackJoon/src/bronze/b2/Main_BJ_1075_나누기.java
@@ -0,0 +1,28 @@
+package bronze.b2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author : jhno96
+ * @date : 2022-05-01
+ */
+public class Main_BJ_1075_나누기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int F = Integer.parseInt(br.readLine());
+
+        int ans = 0;
+        N = N / 100 * 100;
+        while (true) {
+            if((N + ans) % F == 0) break;
+            else ans++;
+        }
+
+        System.out.printf("%02d", ans);
+    }
+
+}
