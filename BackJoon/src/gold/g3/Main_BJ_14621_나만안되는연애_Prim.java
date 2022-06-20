1782e9ca28bc23c006662616a1815f4c80c2e425
jhno96 <jhno96@gmail.com>
2022-06-20 20:56:43 +0900

    0620 스터디

--- BackJoon/src/gold/g3/Main_BJ_14621_나만안되는연애_Prim.java
+++ BackJoon/src/gold/g3/Main_BJ_14621_나만안되는연애_Prim.java
@@ -40,25 +40,29 @@ public class Main_BJ_14621_나만안되는연애_Prim {
 
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
+            // 정점리스트 초기화
             nodeList[i] = new ArrayList<>();
+            // 각 정점의 유형 저장
             isMan[i] = st.nextToken().equals("M");
         }
 
+        // 정점리스트배열 입력
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
+            // 정점의 유형이 서로 다를 때만
             if(isMan[from] != isMan[to]) {
                 nodeList[from].add(new Node(to, w));
                 nodeList[to].add(new Node(from, w));
             }
         }
 
-        int cnt = 0;
-        int ans = 0;
+        int ans = 0;  // 경로의 길이
+        int cnt = 0;  // MST 를 구성하는 정점의 수
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        boolean[] visit = new boolean[N + 1];
+        boolean[] visit = new boolean[N + 1];  // 정점 방문체크
         pq.offer(new Node(1, 0));
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
@@ -73,6 +77,7 @@ public class Main_BJ_14621_나만안되는연애_Prim {
             }
         }
 
+        // MST 를 구성하는 정점의 수를 확인하여 모든 학교를 연결하는 경로가 있는지 판단
         System.out.println(cnt == N ? ans : -1);
     }
 
