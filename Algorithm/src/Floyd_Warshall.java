c44c68362c7fbe2637034f3376a625d11eeaa2ae
jhno96 <53774255+SnowFlower96@users.noreply.github.com>
2022-06-30 21:19:19 +0900

    파일 제거 버전

--- Algorithm/src/Floyd_Warshall.java
+++ Algorithm/src/Floyd_Warshall.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 
 /**
  * 플로이드 와샬 알고리즘
+ *
  * @Author : jhno96
  * @Date : 2022-06-30
  */
@@ -15,29 +16,26 @@ public class Floyd_Warshall {
     static final int INF = 99999999;
 
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("Algorithm/res/Floyd_Warshall_input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         // 정점의 수 입력
-        int N = Integer.parseInt(br.readLine());
-
-        // 인접 행렬 초기화
-        int[][] D = new int[N][N];
+        int N = 4;
 
         // 인접 행렬 입력
+        int[][] D = {{0, 2, 0, 15},
+                     {0, 0, 10, 4},
+                     {3, 0, 0, 0},
+                     {0, 0, 7, 0}};
+
+        // 갈 수 없는 경로 확인
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                D[i][j] = Integer.parseInt(st.nextToken());
-                // 자기자신으로의 인접 정보가 아니고 인접해있지 않다면 INF 로 채우기
-                if (i != j && D[i][j] == 0) D[i][j] = INF;
+                if (i == j) continue;
+                if (D[i][j] == 0) D[i][j] = INF;
             }
         }
 
         // 입력 출력
         System.out.println("=============입력=============");
-        for(int[] row : D) System.out.println(Arrays.toString(row));
+        for (int[] row : D) System.out.println(Arrays.toString(row));
 
         // 플로이드 와샬
         for (int k = 0; k < N; k++) {  // 경유 노드 확인
@@ -52,7 +50,7 @@ public class Floyd_Warshall {
 
         // 결과 출력
         System.out.println("=============결과=============");
-        for(int[] row : D) System.out.println(Arrays.toString(row));
+        for (int[] row : D) System.out.println(Arrays.toString(row));
     }
 
 }
